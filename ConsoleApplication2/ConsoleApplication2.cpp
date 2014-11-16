b277759d5e8a44edb103011b0985547c58740f99
williamscs <cwilliams88@gmail.com>
2014-11-16 02:59:11 -0600

    Improved portability and added transparency

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -6,53 +6,32 @@
 #include <GL/glew.h>
 /* Using the GLUT library for the base windowing setup */
 #include <GL/freeglut.h>
+
+#include "shader_utils.h"
 /* GLOBAL VARIABLES*/
 GLuint program;
 GLint attribute_coord2d;
+GLuint vbo_triangle;
 
-int init_resources(void)
+
+
+int init_resources()
 {
-	GLint compile_ok = GL_FALSE, link_ok = GL_FALSE;
+	GLfloat triangle_vertices[] = {
+		0.0, 0.8,
+		-0.8, -0.8,
+		0.8, -0.8,
+	};
+	glGenBuffers(1, &vbo_triangle);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
 
-	/* Vertex Shader */
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	const char *vs_source =
-#ifdef GL_ES_VERSION_2_0
-		"#version 100\n"  // OpenGL ES 2.0
-#else
-		"#version 120\n"  // OpenGL 2.1
-#endif
-		"attribute vec2 coord2d;                  "
-		"void main(void) {                        "
-		"  gl_Position = vec4(coord2d, 0.0, 1.0); "
-		"}";
-	glShaderSource(vs, 1, &vs_source, NULL);
-	glCompileShader(vs);
-	glGetShaderiv(vs, GL_COMPILE_STATUS, &compile_ok);
-	if (0 == compile_ok)
-	{
-		fprintf(stderr, "Error in vertex shader\n");
-		return 0;
-	}
+	GLint link_ok = GL_FALSE;
 
-	/* Fragment Shader */
-	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	const char *fs_source =
-		"#version 120           \n"
-		"void main(void) {        "
-		"  gl_FragColor[0] = 1.0; "
-		"  gl_FragColor[1] = 0.0; "
-		"  gl_FragColor[2] = 0.0; "
-		"}";
-	glShaderSource(fs, 1, &fs_source, NULL);
-	glCompileShader(fs);
-	glGetShaderiv(fs, GL_COMPILE_STATUS, &compile_ok);
-	if (!compile_ok) {
-		fprintf(stderr, "Error in fragment shader\n");
-		return 0;
-	}
+	GLuint vs, fs;
+	if ((vs = create_shader("triangle.v.glsl", GL_VERTEX_SHADER)) == 0) return 0;
+	if ((fs = create_shader("triangle.f.glsl", GL_FRAGMENT_SHADER)) == 0) return 0;
 
-	/* GLSL Program */
 	program = glCreateProgram();
 	glAttachShader(program, vs);
 	glAttachShader(program, fs);
@@ -60,6 +39,7 @@ int init_resources(void)
 	glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
 	if (!link_ok) {
 		fprintf(stderr, "glLinkProgram:");
+		print_log(program);
 		return 0;
 	}
 
@@ -75,54 +55,37 @@ int init_resources(void)
 
 void onDisplay()
 {
-	/* Clear the background as white */
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	// Enable alpha
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	glUseProgram(program);
 	glEnableVertexAttribArray(attribute_coord2d);
-	GLfloat triangle_vertices[] = {
-		-0.8, 0.8,
-		-0.8, -0.8,
-		0.0, -0.8,
-	};
 	/* Describe our vertices array to OpenGL (it can't guess its format automatically) */
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle);
 	glVertexAttribPointer(
 		attribute_coord2d, // attribute
 		2,                 // number of elements per vertex, here (x,y)
 		GL_FLOAT,          // the type of each element
 		GL_FALSE,          // take our values as-is
 		0,                 // no extra data between each position
-		triangle_vertices  // pointer to the C array
-		);
-
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	GLfloat triangle_vertices2[] = {
-		-0.8, 0.8,
-		0.0, 0.8,
-		0.0, -0.8,
-	};
-	/* Describe our vertices array to OpenGL (it can't guess its format automatically) */
-	glVertexAttribPointer(
-		attribute_coord2d, // attribute
-		2,                 // number of elements per vertex, here (x,y)
-		GL_FLOAT,          // the type of each element
-		GL_FALSE,          // take our values as-is
-		0,                 // no extra data between each position
-		triangle_vertices2  // pointer to the C array
+		0                  // offset of first element
 		);
 
 	/* Push each element in buffer_vertices to the vertex shader */
 	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDisableVertexAttribArray(attribute_coord2d);
 
-	/* Display the result */
+	glDisableVertexAttribArray(attribute_coord2d);
 	glutSwapBuffers();
 }
 
 void free_resources()
 {
 	glDeleteProgram(program);
+	glDeleteBuffers(1, &vbo_triangle);
 }
 
 int main(int argc, char* argv[])
@@ -130,7 +93,7 @@ int main(int argc, char* argv[])
 	/* Glut-related initialising functions */
 	glutInit(&argc, argv);
 	glutInitContextVersion(2, 0);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowSize(640, 480);
 	glutCreateWindow("My First Triangle");
 
@@ -140,6 +103,10 @@ int main(int argc, char* argv[])
 	{
 		fprintf(stderr, "Error: %s\n", glewGetErrorString(glew_status));
 		return EXIT_FAILURE;
+	}
+	if (!GLEW_VERSION_2_0) {
+		fprintf(stderr, "Error: your graphic card does not support OpenGL 2.0\n");
+		return 1;
 	}
 
 	/* When all init functions run without errors,
